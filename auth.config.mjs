bb54ec3a69f09cd445ff68d5f54f277c525878bf
Raffyshira <rafiahsiraprayoga@gmail.com>
2024-09-24 19:06:10 +0700

    add navbar desktop

--- auth.config.mjs
+++ auth.config.mjs
@@ -1,11 +1,15 @@
-import GitHub from '@auth/core/providers/github';
-import { defineConfig } from 'auth-astro';
+import GitHub from "@auth/core/providers/github";
+import { defineConfig } from "auth-astro";
 
 export default defineConfig({
-  providers: [
-    GitHub({
-      clientId: import.meta.env.GITHUB_CLIENT_ID,
-      clientSecret: import.meta.env.GITHUB_CLIENT_SECRET,
-    }),
-  ],
+    providers: [
+        GitHub({
+            clientId: import.meta.env.GITHUB_CLIENT_ID,
+            clientSecret: import.meta.env.GITHUB_CLIENT_SECRET,
+        })
+    ],
+    cookies: {
+    secure: 'production',  // Aktifkan secure cookie di production
+    sameSite: 'none',  // Memungkinkan cookie lintas situs untuk OAuth
+  },
 });
--- src/components/mobile/navbar/list-navbar.jsx
+++ src/components/mobile/navbar/list-navbar.jsx
@@ -1,11 +1,11 @@
 import { MenuItems } from "@/lib/nav-menu.jsx";
 
-export const ListNavbar = ({start, end}) => {
+export const ListNavbar = ({ start, end }) => {
     return (
         <>
             {MenuItems.slice(start, end).map(item => (
                 <a
-                    class="inline-flex items-center gap-x-3 mt-3.5"
+                    className="inline-flex items-center gap-x-3 mt-3.5"
                     href={item.link}
                 >
                     {item.icon}
